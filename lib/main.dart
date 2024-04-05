907fec0cf86356d9bba6494a1b1ae9362c48edba
Mark Hentges <hentg089@umn.edu>
2024-04-05 16:48:48 -0500

    h

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
 import 'pages/notification_page.dart';
 import 'pages/home_page.dart';
-import 'pages/tip_page.dart';
+import 'pages/camera_page.dart';
 import 'pages/sign_up_page.dart';
 import 'pages/HealthyEating_page.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: HomePage(),
       routes: {
         '/homepage': (context) => HomePage(),
         '/loginpage': (context) => LoginPage(),
