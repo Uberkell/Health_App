d268c23cc72eed70085f4e13acfbf5244ee723b4
will class <class054@d.umn.edu>
2024-04-08 20:48:38 -0500

    Created a simple static chart, does nothing, will work on formating it so that it wont take up the whole page like it currently does along with a ton of other things.

---
+++ lib/pages/Graph_Page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+
+class GraphTest extends StatelessWidget {
+  GraphTest({Key? key}) : super(key: key);
+
+  @override
+  GraphTest createState() => GraphTest();
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test Graph'),
+        ),
+        body: LineChartWidget(),
+      ),
+    );
+  }
+}
+
+class LineChartWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<MyData> data = [
+      MyData(DateTime(2022, 1, 1), 10),
+      MyData(DateTime(2022, 1, 2), 20),
+      MyData(DateTime(2022, 1, 3), 15),
+      MyData(DateTime(2022, 1, 4), 25),
+      MyData(DateTime(2022, 1, 5), 30),
+    ];
+
+    final List<charts.Series<MyData, DateTime>> seriesList = [
+      charts.Series<MyData, DateTime>(
+        id: 'Data',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (MyData data, _) => data.timestamp,
+        measureFn: (MyData data, _) => data.value,
+        data: data,
+      ),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: charts.TimeSeriesChart(
+        seriesList,
+        animate: true,
+        dateTimeFactory: const charts.LocalDateTimeFactory(),
+      ),
+    );
+  }
+}
+
+class MyData {
+  final DateTime timestamp;
+  final int value;
+
+  MyData(this.timestamp, this.value);
+}
