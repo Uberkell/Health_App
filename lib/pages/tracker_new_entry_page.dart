2708fba38969396626f4048414ae72f6acaad834
Katie Allen <alle1313@umn.edu>
2024-04-09 17:47:47 -0500

    Working firebase to add new food

--- lib/pages/tracker_new_entry_page.dart
+++ lib/pages/tracker_new_entry_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TrackerNewEntryPage extends StatelessWidget {
   TrackerNewEntryPage({Key? key}) : super(key: key);
@@ -29,6 +29,26 @@ class MyCustomForm extends StatefulWidget {
 class MyCustomFormState extends State<MyCustomForm> {
   // Create a global key that uniquely identifies the Form widget
   final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final calsController = TextEditingController();
+  final proteinController = TextEditingController();
+  final vegController = TextEditingController();
+  final fruitController = TextEditingController();
+  final sodiumController = TextEditingController();
+  final waterController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    calsController.dispose();
+    proteinController.dispose();
+    sodiumController.dispose();
+    vegController.dispose();
+    fruitController.dispose();
+    waterController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,71 +56,123 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Form(
       key: _formKey,
 
-
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
                 decoration: InputDecoration(labelText: "Name of Food"),
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.text,
+                controller: nameController,
               )),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
                 decoration: InputDecoration(labelText: "Calories Consumed Today"),
-                keyboardType: TextInputType.number
+                keyboardType: TextInputType.number,
+                controller: calsController,
               /*onChanged: (value) {};*/
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Protein in Grams Consumed Today"),
-                  keyboardType: TextInputType.number),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Protein in Grams Consumed Today"),
+                keyboardType: TextInputType.number,
+                controller: proteinController,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Sodium in Grams Consumed Today"),
-                  keyboardType: TextInputType.number),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Sodium in Grams Consumed Today"),
+                keyboardType: TextInputType.number,
+                controller: sodiumController,),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Servings of Fruit Consumed Today"),
-                keyboardType: TextInputType.number),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Servings of Fruit Consumed Today"),
+                keyboardType: TextInputType.number,
+                controller: fruitController,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Servings of Vegetables Consumed Today"),
-                  keyboardType: TextInputType.number),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(labelText: "Servings of Vegetables Consumed Today"),
+                keyboardType: TextInputType.number,
+                controller: vegController,),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 15),
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.red),
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-              ),
-              onPressed: () {
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                onPressed: () async {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    String message;
+                    try {
+                      // Get a reference to the `food` collection
+                      final collection = FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser?.uid).collection('food').doc(nameController.text);
 
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Uploading Data')),
-                  );
-                }
-              },
-          child: const Text('Submit'),
+                      // Write the server's timestamp and the user's feedback
+                      await collection.set({
+                        'calories': calsController.text,
+                        'protein': proteinController.text,
+                        'sodium': sodiumController.text,
+                        'fruit': fruitController.text,
+                        'vegetables': vegController.text,
+                        'water': waterController.text,
+                      });
+
+                      message = 'Entry sent successfully';
+                    } catch (e) {
+                      message = 'Error when sending entry';
+                    }
+
+                    // Show a snackbar with the result
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text(message)));
+                    Navigator.pop(context);
+                  }
+
+                },
+                child: const Text('Submit'),
         ),
       ),
     ]
