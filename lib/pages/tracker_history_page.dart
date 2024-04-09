2b2d7eca41fbe020a20f2278174fc0ea6841b7d3
Katie Allen <alle1313@umn.edu>
2024-04-09 15:59:06 -0500

    Changed some programming structures and began firebase integration

--- lib/pages/tracker_history_page.dart
+++ lib/pages/tracker_history_page.dart
@@ -1,28 +1,63 @@
-// this is where the tracking journal button will display past entries
 import 'package:flutter/material.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TrackerHistoryPage extends StatelessWidget {
-  TrackerHistoryPage({Key? key}) : super(key: key);
+  const TrackerHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Text calsConsumed = new Text(
-      labelText: "Calories Consumed"
-    );
-    Text waterConsumed = new Text(
-        labelText: "Water Consumed"
-    );
-    Text proteinConsumed = new Text(
-        labelText: "Protein Consumed"
-    );
-    Text sodiumConsumed = new Text(
-        labelText: "Sodium Consumed"
-    );
-    Text fruitsConsumed = new Text(
-        labelText: "Fruits Consumed"
-    );
-    Text vegsConsumed = new Text(
-        labelText: "Vegetables Consumed"
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("What did you eat today?"),
+        ),
+        body: Text("History") //AddData()
     );
   }
 }
+
+  /*
+  Future<List<Widget>> retrieveHistory() async {
+    final userid = FirebaseAuth.instance.currentUser?.uid;
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(userid)
+        .collection('Meal Tracking')
+        .get();
+    List<Widget> pastFoods = [];
+  }
+}
+class AddData extends StatelessWidget {
+
+   @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+           backgroundColor: Colors.green,
+
+        title: Text("geeksforgeeks"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('data').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+               if (!snapshot.hasData){
+
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+}
+          return ListView(
+            children: snapshot.data.docs.map((document) {
+                       return Container(
+                child: Center(child: Text(document['text'])),
+              );
+              }).toList(),
+          );},
+      ),
+    );
+    }
+
+} */
