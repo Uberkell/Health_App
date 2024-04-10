4bed957234c6aa87df66abcc477775d28f7df35c
will class <class054@d.umn.edu>
2024-04-09 23:46:04 -0500

    reworked the sign in page a bit back arrow wasesnt working and i couldent find a fix for how it was, looked at what Jiwon did and adapted it for my own dark purposes :)

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,94 +1,105 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpPage extends StatelessWidget {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  SignUpPage({super.key});
+
+  SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            //back to prev page button
-            IconButton(
-              padding: EdgeInsets.fromLTRB(0, 0,400, 0),
-              icon: const Icon(Icons.arrow_back),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 2,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back),
+              color: Colors.red,
               iconSize: 50,
-              onPressed: () => {Navigator.pop(context)},
-              color: Colors.grey,
             ),
-            SizedBox(height: 100),
-            //welcome
-            Text(
-              'Sign Up',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25
+          ),
+          Center( // Centering the content
+            child: SingleChildScrollView( // Making content scrollable
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // Centering content vertically
+                crossAxisAlignment: CrossAxisAlignment.center, // Centering content horizontally
+                children: [
+                  Container (
+                    height: 100,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                    ),
+                  ),
+                  ),
+                  SizedBox(height: 50),
+                  TextFieldSignIn(
+                    controller: usernameController,
+                    hintText: 'Username',
+                    obscureText: false,
+                  ),
+                  SizedBox(height: 20), // Reduced the height for better spacing
+                  TextFieldSignIn(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20), // Reduced the height for better spacing
+                  CreateAccountButton(
+                    usernameController: usernameController,
+                    passwordController: passwordController,
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 50),
-            //username textfield
-            TextFieldSignIn(
-              controller: usernameController,
-              hintText: 'Username',
-              obscureText: false,
-            ),
-            SizedBox(height: 50),
-            //password textfield
-            TextFieldSignIn(
-              controller: passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            SizedBox(height: 50),
-            CreateAccountButton(
-              usernameController: usernameController,
-              passwordController: passwordController,
-            ),
-          ],
-        )
+          ),
+        ],
+      ),
     );
   }
 }
 
-class TextFieldSignIn extends StatelessWidget{
-  final controller;
+class TextFieldSignIn extends StatelessWidget {
+  final TextEditingController controller;
   final String hintText;
   final bool obscureText;
 
   const TextFieldSignIn({
-    super.key,
+    Key? key,
     required this.controller,
     required this.hintText,
     required this.obscureText,
-  });
-
-
-
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: TextField(
-          controller: controller,
-          obscureText: obscureText,
-          decoration: InputDecoration(
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
           ),
-        )
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
     );
   }
 }
@@ -103,52 +114,52 @@ class CreateAccountButton extends StatelessWidget {
     required this.passwordController,
   }) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          signUp(
-            usernameController.text.trim(),
-            passwordController.text.trim(),
-          );
-        },
-        child: Container(
-          padding: const EdgeInsets.all(25),
-          margin: const EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: const Center(
-            child: Text(
-                "Create Account",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                )
+      onTap: () {
+        signUp(
+          usernameController.text.trim(),
+          passwordController.text.trim(),
+          context,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15), // Reduced padding for better appearance
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center( // Centering the text
+          child: Text(
+            "Create Account",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
             ),
           ),
-        )
+        ),
+      ),
     );
   }
-  Future<void> signUp(String email, String password) async {
+
+  Future<void> signUp(String email, String password, BuildContext context) async {
     try {
       UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
       if (userCredential != null) {
-        FirebaseFirestore.instance.collection('Users').doc((userCredential.user?.uid).toString()).set({});
+        await FirebaseFirestore.instance.collection('Users').doc(userCredential.user?.uid).set({});
+        // Optionally, you can navigate to another page after successful signup
+        // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AnotherPage()));
       }
-      // Optionally, you can navigate to another page after successful signup
-      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AnotherPage()));
     } catch (e) {
       print(e.toString());
-      print("rip");
       // Handle error, for example, show error message
-      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign up failed')));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign up failed')));
     }
   }
 }
