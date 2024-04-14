6b43a148bc7d4c7a071afe793f64f301defba694
will class <class054@d.umn.edu>
2024-04-14 13:45:26 -0500

    Created a basic line chart, need to work on size customization and a lot of other things, but it works.

--- lib/pages/Graph_Page.dart
+++ lib/pages/Graph_Page.dart
@@ -10,56 +10,39 @@ class GraphTest extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Syncfusion Line Chart Test',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Test Graph'),
+          title: Text('Syncfusion Line Chart Test'),
         ),
-        body: LineChartWidget(),
-      ),
-    );
-  }
-}
-
-class LineChartWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final List<MyData> data = [
-      MyData(DateTime(2024, 4, 1), 10),
-      MyData(DateTime(2024, 4, 2), 20),
-      MyData(DateTime(2024, 4, 3), 15),
-      MyData(DateTime(2024, 4, 4), 25),
-      MyData(DateTime(2024, 4, 5), 30),
-    ];
-
-    final List<charts.Series<MyData, DateTime>> seriesList = [
-      charts.Series<MyData, DateTime>(
-        id: 'Data',
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (MyData data, _) => data.timestamp,
-        measureFn: (MyData data, _) => data.value,
-        data: data,
-      ),
-    ];
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 300, // height adjustment
-        width: double.infinity, // Width adjustment
-        child: charts.TimeSeriesChart(
-          seriesList,
-          animate: true,
-          dateTimeFactory: const charts.LocalDateTimeFactory(),
+        body: Center(
+          child: Container(
+            height: 300,
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              series: <LineSeries<CalorieData, String>>[
+                LineSeries<CalorieData, String>(
+                  dataSource: <CalorieData>[
+                    CalorieData('Jan', 35),
+                    CalorieData('Feb', 28),
+                    CalorieData('Mar', 34),
+                    CalorieData('Apr', 32),
+                    CalorieData('May', 40),
+                  ],
+                  xValueMapper: (CalorieData calories, _) => calories.month,
+                  yValueMapper: (CalorieData calories, _) => calories.calories,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
-
-class MyData {
-  final DateTime timestamp;
-  final int value;
-
-  MyData(this.timestamp, this.value);
+class CalorieData {
+  CalorieData(this.month, this.calories);
+  final String month;
+  final double calories;
 }
