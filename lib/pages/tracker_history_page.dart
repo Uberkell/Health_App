f85a6c806eb4669f66b60eedb55858cf7718a82e
Katie Allen <alle1313@umn.edu>
2024-04-24 03:05:49 -0500

    displays data on History page, goal progress bars were added, water entry into firestore has been updated

--- lib/pages/tracker_history_page.dart
+++ lib/pages/tracker_history_page.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -10,9 +10,143 @@ class TrackerHistoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("What did you eat today?"),
+          title: const Text("Past History"),
         ),
-        body: Text("History") //AddData()
-    );
+        body: UpdatingTrackerHistoryPage() //AddData()
+        );
   }
+}
+
+class UpdatingTrackerHistoryPage extends StatefulWidget {
+  const UpdatingTrackerHistoryPage({super.key});
+
+  @override
+  UpdatingTrackerHistoryPageState createState() {
+    return UpdatingTrackerHistoryPageState();
+  }
+}
+
+class UpdatingTrackerHistoryPageState extends State<UpdatingTrackerHistoryPage> {
+  final usersCollection = FirebaseFirestore.instance.collection('Users');
+  final userId = FirebaseAuth.instance.currentUser?.uid;
+
+  Future<List<Widget>> getData() async {
+    final userid = FirebaseAuth.instance.currentUser?.uid;
+
+    QuerySnapshot query = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(userid)
+        .collection("Dates").get();
+
+    List<Widget> food = [];
+
+    await Future.forEach(query.docs, (days) async{
+
+      QuerySnapshot queryQuestion = await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userid)
+          .collection("Dates")
+          .doc("4-24-2024") //4-22-2024 rn need to be all
+          .collection("Food_and_Water")
+          .get();
+
+        List<Widget> tempfood = [];
+        await Future.forEach(queryQuestion.docs, (doc) async {
+          Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+
+          if (data != null && data.length > 3) {
+            var tempSodium = data.entries.first;
+            var tempfruit = data.entries.elementAt(1);
+            var tempcal = data.entries.elementAt(2);
+            var tempprotein = data.entries.elementAt(3);
+            var tempveg = data.entries.elementAt(4);
+
+            tempfood.add(FoodData(
+                doc.id, //food name
+                days.id, // date
+                displayPair(tempSodium.key, tempSodium.value),
+                displayPair(tempprotein.key, tempprotein.value),
+                displayPair(tempfruit.key, tempfruit.value),
+                displayPair(tempveg.key, tempveg.value),
+                displayPair(tempcal.key, tempcal.value)
+            ));
+            setState(() {
+
+            });
+          } else if (data != null && data.length < 3){
+            var tempWater = data.entries.first;
+            food.add(WaterData(doc.id, days.id, displayPair("Water drank", "${tempWater.value} cups")));
+          }
+        });
+
+        for (var entry in tempfood) {
+          food.add(entry);
+         }
+
+    });
+    return food;
+  }
+
+  String displayPair(var label, var value){
+    return "$label: $value";
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Widget>>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          return SingleChildScrollView(
+            child: Column(
+              children: snapshot.data!,
+            ),
+          );
+        });
+  }
+}
+
+class FoodData extends StatelessWidget {
+  FoodData(this.name, this.date, this.sodium, this.fruit,
+      this.cals, this.protein, this.veg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(name),
+              Text(date),
+              Text(cals),
+              Text(protein),
+              Text(sodium),
+              Text(veg),
+              Text(fruit),
+            ]));
+  }
+
+  final String date, name, cals, sodium, protein, fruit, veg;
+}
+
+class WaterData extends FoodData {
+  WaterData(this.water, this.date, this.amount) : super('', '', '', '', '', '', '');
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(date),
+              Text(amount.toString())
+            ]));
+  }
+
+  final String amount, water;
+  @override
+  final String date;
 }
--- lib/pages/tracker_home_page.dart
+++ lib/pages/tracker_home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:int_to_win_it/pages/Graph_Page.dart';
 import 'package:int_to_win_it/pages/tracker_new_entry_page.dart';
 import 'package:int_to_win_it/pages/tracker_history_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,10 +9,34 @@ class TrackerHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("What did you eat today?"),
+        ),
+        body: Scaffold(body: UpdatingTrackerHomePage()));
+  }
+}
 
+class UpdatingTrackerHomePage extends StatefulWidget {
+  const UpdatingTrackerHomePage({super.key});
+
+  @override
+  UpdatedState createState() {
+    return UpdatedState();
+  }
+}
+
+class UpdatedState extends State<UpdatingTrackerHomePage> {
+  // Create a global key that uniquely identifies the Form widget
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     ElevatedButton addFood = ElevatedButton(
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => TrackerNewEntryPage()));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => TrackerNewEntryPage()));
       },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(Colors.green),
@@ -22,7 +45,7 @@ class TrackerHomePage extends StatelessWidget {
       child: const Text("Add Food"),
     );
 
-    String todayDate(){
+    String todayDate() {
       DateTime today = DateTime.now();
       String dateStr = "${today.month}-${today.day}-${today.year}";
 
@@ -31,6 +54,7 @@ class TrackerHomePage extends StatelessWidget {
 
     TextEditingController waterController = TextEditingController();
     TextEditingController unitController = TextEditingController();
+    int waterIndex = 0;
 
     ElevatedButton addWater = ElevatedButton(
       onPressed: () {
@@ -51,33 +75,36 @@ class TrackerHomePage extends StatelessWidget {
                         ),
                           ElevatedButton(
                               style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.green),
-                                foregroundColor: MaterialStateProperty.all(Colors.white),
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.green),
+                                foregroundColor: MaterialStateProperty.all(
+                                    Colors.white),
                               ),
                               onPressed: () async {
                                 String message;
                                 try {
-                                  //final collection = FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser?.uid).collection(todayDate()).doc("water");
-                                  final usersCollection = FirebaseFirestore.instance.collection('Users');
-                                  final userId = FirebaseAuth.instance.currentUser?.uid;
-                                  final userDocRef = usersCollection.doc(userId);
-                                  final dateEntry = todayDate();
-                                  final userDateCollection = userDocRef.collection('Dates').doc(dateEntry);
-                                  final foodCollection = userDateCollection.collection('Food_and_Water');
-                                  final foodItem =foodCollection.doc('water');
-                                  await foodItem.set({
+              final usersCollection = FirebaseFirestore.instance.collection('Users');
+              final userId = FirebaseAuth.instance.currentUser?.uid;
+              final userDocRef = usersCollection.doc(userId);
+              final dateEntry = todayDate();
+              final userDateCollection = userDocRef.collection('Dates').doc(dateEntry);
+              final foodCollection = userDateCollection.collection('Food_and_Water');
+                                  final collection = foodCollection.doc("water $waterIndex");
+                                  await collection.set({
                                     'waterDrank': waterController.text,
                                     'units': unitController.text,
                                   }
                                   );
                                   message = 'Entry sent successfully';
+                                  waterIndex += 1;
                                 } catch (e) {
                                   message = 'Error when sending entry';
                                 }
                                 // Show a snackbar with the result
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text(message)));
                                 Navigator.pop(context);
-                                },
+                              },
                               child: const Text("Enter")),
                         ]
                     ),
@@ -96,25 +123,23 @@ class TrackerHomePage extends StatelessWidget {
 
     FloatingActionButton seeHistory = FloatingActionButton.extended(
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => TrackerHistoryPage()));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => TrackerHistoryPage()));
       },
       backgroundColor: Colors.green,
       foregroundColor: Colors.white,
-      label: const Text("Past History"),
-    );
-
-    FloatingActionButton seeGraph = FloatingActionButton.extended(
-      onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => GraphTest()));
-      },
-      backgroundColor: Colors.green,
-      foregroundColor: Colors.white,
-      label: const Text("See Graph"),
+      label: const Text("See Past History"),
     );
 
     String curGoal = "Calories";
     final goalController = TextEditingController();
-    List<String> diffGoals = ["Calories", "Protein", "Vegetables", "Fruit", "Sodium"];
+    List<String> diffGoals = [
+      "Calories",
+      "Protein",
+      "Vegetables",
+      "Fruit",
+      "Sodium"
+    ];
     ElevatedButton setGoal = ElevatedButton(
       onPressed: () {
         showDialog(context: context,
@@ -132,7 +157,8 @@ class TrackerHomePage extends StatelessWidget {
                             onChanged: (String? newValue) {
                               curGoal = newValue!;
                             },
-                            items:  diffGoals.map<DropdownMenuItem<String>>((String value) {
+                            items: diffGoals.map<DropdownMenuItem<String>>((
+                                String value) {
                               return DropdownMenuItem<String>(
                                 value: value,
                                 child: Text(value),
@@ -147,14 +173,18 @@ class TrackerHomePage extends StatelessWidget {
                                   }
                                   return null;
                                 },
-                                decoration: InputDecoration(label: Center(child: Text("New Goal"))),
+                                decoration: InputDecoration(
+                                    label: Center(child: Text("New Goal"))),
                                 keyboardType: TextInputType.number,
                                 controller: goalController,
-                              )),]),
+                              )),
+                        ]),
                           ElevatedButton(
                               style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.green),
-                                foregroundColor: MaterialStateProperty.all(Colors.white),
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.green),
+                                foregroundColor: MaterialStateProperty.all(
+                                    Colors.white),
                               ),
                               onPressed: () {
                                 Navigator.pop(context);
@@ -175,37 +205,90 @@ class TrackerHomePage extends StatelessWidget {
       child: const Text("Set Goals"),
     );
 
+    CircularProgressIndicator calProgress = CircularProgressIndicator(
+      value: 10/100,
+    color: Colors.green,);
+
+    CircularProgressIndicator waterProgress = CircularProgressIndicator(
+      value: 20/100,
+        color: Colors.green);
+
+    CircularProgressIndicator sodiumProgress = CircularProgressIndicator(
+      value: 30/100,
+        color: Colors.green);
+
+    CircularProgressIndicator proteinProgress = CircularProgressIndicator(
+      value: 40/100,
+        color: Colors.green);
+
+    CircularProgressIndicator fruitProgress = CircularProgressIndicator(
+      value: 50/100,
+        color: Colors.green);
+
+    CircularProgressIndicator vegProgress = CircularProgressIndicator(
+      value: 60/100,
+        color: Colors.green);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("What did you eat today?"),
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [addFood, addWater, setGoal],
-          ),
-          Expanded(child: Container()), // Placeholder to push other content to the bottom
-        ],
-      ),
-      floatingActionButton: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 30.0, bottom: 16.0),
-              child: seeHistory,
+      body: Stack(
+          children: [
+            ListView( children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [addFood, addWater, setGoal]),
+            Padding(padding: EdgeInsets.symmetric(vertical: 60),
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Calories"),
+                        calProgress])),
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Water"),
+                        waterProgress])),
+                ],),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.only( right: 3.0, bottom: 16.0),
-              child: seeGraph,
+            Padding(padding: EdgeInsets.symmetric(vertical: 60),
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Sodium"),
+                        sodiumProgress])),
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Protein"),
+                        proteinProgress])),
+                ],),
             ),
-          ),
-        ],
-      ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 60),
+              child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Vegetables"),
+                        vegProgress])),
+                  Transform.scale(
+                      scale: 2,
+                      child: Column(children: [
+                        Text("Fruit"),
+                        fruitProgress])),
+                ],),
+            ),
+          ]
+      )]),
+      floatingActionButton: seeHistory,
     );
   }
 }
